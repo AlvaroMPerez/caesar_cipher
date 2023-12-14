ec0222d07c0c144c101f43b6f0b15f7da8f59dc9
lilvallo <x_vallo_x@hotmail.com>
2023-12-14 14:12:21 -0600

    hola

---
+++ main.rb
@@ -0,0 +1,7 @@
+def  caesar_cipher(text, number)
+    cipher = ""
+    lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    upper_alphabet = lower_alphabet.upcase
+    n =  lower_alphabet.length
+
+end
