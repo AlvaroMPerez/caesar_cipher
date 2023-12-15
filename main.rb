f746d70d9643a1d070186eeb90aa9cccf32206db
Vallo <x_vallo_x@hotmail.com>
2023-12-15 10:34:37 -0600

    cipher en minusculas

--- main.rb
+++ main.rb
@@ -1,7 +1,17 @@
-def  caesar_cipher(text, number)
-    cipher = ""
-    lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
-    upper_alphabet = lower_alphabet.upcase
-    n =  lower_alphabet.length
 
-end
+def caesar_cipher(text, number)
+    cifrado = ""
+    text.each_char do |letra|
+        if letra.match?(/[a-zA-Z]/)
+            letra_ascii_numero = letra.ord
+            nueva_letra_ascii = (letra_ascii_numero + number - 'a'.ord)%26 + 'a'.ord
+            letra_cifrada = nueva_letra_ascii.chr
+            cifrado += letra_cifrada
+        else
+            cifrado += letra
+        end
+    end
+    return cifrado
+  end
+
+  puts caesar_cipher("Abc",3)
